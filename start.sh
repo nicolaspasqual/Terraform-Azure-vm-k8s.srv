804ec352b12f81d6e772b99a8c3b68ea7dea1ba2
Nicolas Pasqual <nicolaspasqual92@gmail.com>
2023-06-07 17:53:15 -0300

    ajuste folder

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm ./ansible/hosts
-cd ./terraform
-terraform output public_ip_address_1 | sed 's/^"//' | sed 's/"$//' >> ../ansible/hosts
-terraform output public_ip_address_2 | sed 's/^"//' | sed 's/"$//' >> ../ansible/hosts
